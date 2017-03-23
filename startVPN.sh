0a4ea0ae576ce3f447444584e65ae84150a3882f
Andreas Prang <mail@andreasprang.de>
2017-03-23 13:02:24 +0100

    startVPN.sh

---
+++ startVPN.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# install docker on debian jessie if needed
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+
+if exists docker; then
+    docker -v
+else
+    echo "Installing Docker"
+    sudo apt-get -y install      apt-transport-https      ca-certificates      curl      software-properties-common
+	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+	sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+	sudo apt-get update
+	sudo apt-get install docker-ce
+	clear
+	docker -v 
+fi
+
+
+# set credentials
+echo -n User: 
+read VPN_USER
+echo -n Password: 
+read VPN_PASSWORD
+echo -n IPSEC PSK: 
+read VPN_IPSEC_PSK
+echo
+echo
+
+touch vpn.env
+echo "VPN_USER=$VPN_USER" > vpn.env
+echo "VPN_PASSWORD=$VPN_PASSWORD" >> vpn.env
+echo "VPN_IPSEC_PSK=$VPN_IPSEC_PSK" >> vpn.env
+
+
+sudo modprobe af_key
+
+# get Docker Image
+docker pull hwdsl2/ipsec-vpn-server
+
+# start container
+docker run \
+    --name ipsec-vpn-server \
+    --env-file ./vpn.env \
+    --restart=always \
+    -p 500:500/udp \
+    -p 4500:4500/udp \
+    -v /lib/modules:/lib/modules:ro \
+    -d --privileged \
+    hwdsl2/ipsec-vpn-server
+    
+    
+    
